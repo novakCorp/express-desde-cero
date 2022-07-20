d66979a7084730199fd0a0992d61a7403620c411
nova <nova.corps1010@outlook.com>
2022-07-20 14:47:18 -0400

    metodos

--- index.js
+++ index.js
@@ -6,11 +6,38 @@ app.set("port", 3000);
 app.set("title", "Aplicacion con express");
 
 // Routes
-app.get("/", (req, res) => {
-    console.log(req.url);
-    console.log(req.method);
-    res.send("Accesing to the first route.")
-})
+
+app.all("/users", (req, res, next) => {
+    console.log("Primer URL");
+    next();
+});
+
+
+// get
+app.get("/users", (req, res) => {
+    res.send("Accesing to the first route, get.")
+});
+
+// post
+app.post("/users", (req, res)=> {
+    res.send("Accesing to the first route, post, post.")
+});
+
+// put
+app.put("/users", (req, res)=> {
+    res.send("Accesing to the first route, put.")
+});
+
+// patch
+app.patch("/users", (req, res)=> {
+    res.send("Accesing to the first route, patch.")
+});
+
+// delete
+app.delete("/users", (req, res)=> {
+    res.send("Accesing to the first route, delete.")
+});
+
 
 app.listen(app.get("port"), ()=> {
     console.log(`${app.get("title")}, aplicacion corriendo en el puerto: ${app.get("port")}`);
