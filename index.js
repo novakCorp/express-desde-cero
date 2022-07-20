b064b6ec4444ef9c88a11bdbfe46252834294fc5
nova <nova.corps1010@outlook.com>
2022-07-20 12:50:48 -0400

    direccionamiento basico

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const app = express();
 app.set("port", 3000);
 app.set("title", "Aplicacion con express");
 
+// Routes
+app.get("/", (req, res) => {
+    console.log(req.url);
+    console.log(req.method);
+    res.send("Accesing to the first route.")
+})
+
 app.listen(app.get("port"), ()=> {
     console.log(`${app.get("title")}, aplicacion corriendo en el puerto: ${app.get("port")}`);
 });
