5cabc038592b21f18b8f1526c202b182f8e9a4ff
nova <nova.corps1010@outlook.com>
2022-07-20 18:13:55 -0400

    middleware de nivel de aplicacion

--- index.js
+++ index.js
@@ -6,11 +6,25 @@ const app = express();
 app.set("port", 3000);
 app.set("title", "Aplicacion con express");
 
+// Middlewares
+const logger = (req, res, next) => {
+    const userLogged = false;
+    if (userLogged) {
+        console.log("dentro del middleware");
+        next();    
+    }
+    else {
+        res.send("No puede acceder")
+    }  
+};
+
 // Routes
-// routes from an external file
-app.use("/admin", userRoutes);
+app.use(userRoutes);
 
-
+// Applying the middleware
+app.get("/admin", logger, (req, res) => {
+    res.send("Panel de administracion")
+});
 
 app.listen(app.get("port"), ()=> {
     console.log(`${app.get("title")}, aplicacion corriendo en el puerto: ${app.get("port")}`);
