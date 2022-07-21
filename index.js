1afd34663e2942ea0b3b002dd1ab5be9c9fcedd0
nova <nova.corps1010@outlook.com>
2022-07-20 20:30:17 -0400

    Middlewares de terceros

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const path = require("path");
 const userRoutes = require("./routes/users");
 const indexRoutes = require("./routes/index");
+const morgan = require("morgan");
 const app = express();
 
 // Settings
@@ -8,21 +10,25 @@ app.set("port", 3000);
 app.set("title", "Aplicacion con express");
 
 // Middlewares
-const errorHandling = (err, req, res, next) => {
-    res.status(500).send("Ha ocurrido un error, maminga");
-};
+app.use(express.static(path.join(__dirname, "public")));
+// parse encoded body data
+app.use(express.urlencoded({extended: false}));
+app.use(morgan("dev"));
 
 
 // Routes
 app.use(indexRoutes);
 app.use(userRoutes);
 app.get("/admin", (req, res) => {
-    const name = req.params.name
+    const name = req.params.name;
     res.send("Panel de administracion")
 });
 
-// Middlewares that handle errores must be placed after all the routes, at the end.
-app.use(errorHandling);
+app.post("/admin", (req, res) => {
+    let name = req.body.name;
+    console.log(name);
+    res.send("Usuario registrado");
+})
 
 app.listen(app.get("port"), ()=> {
     console.log(`${app.get("title")}, aplicacion corriendo en el puerto: ${app.get("port")}`);
