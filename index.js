fc4985a423066fb17c741574d3e1c1c0492e87f9
nova <nova.corps1010@outlook.com>
2022-07-20 20:08:31 -0400

    Middlwares de manejos de errores

--- index.js
+++ index.js
@@ -8,15 +8,21 @@ app.set("port", 3000);
 app.set("title", "Aplicacion con express");
 
 // Middlewares
-
+const errorHandling = (err, req, res, next) => {
+    res.status(500).send("Ha ocurrido un error, maminga");
+};
 
 
 // Routes
 app.use(indexRoutes);
 app.use(userRoutes);
 app.get("/admin", (req, res) => {
+    const name = req.params.name
     res.send("Panel de administracion")
 });
+
+// Middlewares that handle errores must be placed after all the routes, at the end.
+app.use(errorHandling);
 
 app.listen(app.get("port"), ()=> {
     console.log(`${app.get("title")}, aplicacion corriendo en el puerto: ${app.get("port")}`);
